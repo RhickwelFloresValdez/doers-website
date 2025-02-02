0052ea107032e2ee5f0f9f839dead2fe10ab9ba6
rhickwelfloresvaldez <rhickwelfloresvaldez@gmail.com>
2025-02-02 23:56:21 +0800

    added animations for mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 function showMobileNav() {
     const mobileNav = document.querySelector('.mobile-nav');
     mobileNav.style.display = 'flex';
+    mobileNav.classList.add('slide-in-top');
 }
 
 function hideMobileNav() {
     const mobileNav = document.querySelector('.mobile-nav');
-    mobileNav.style.display = 'none';
+    mobileNav.classList.add('slide-out-top');
+
+    mobileNav.addEventListener('animationend', function () {
+        mobileNav.style.display = 'none';
+        mobileNav.classList.remove('slide-out-top');
+    }, { once: true });
 }
