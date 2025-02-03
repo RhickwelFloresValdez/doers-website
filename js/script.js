3d097435a38cb1627288da9025232f20ee390ffa
rhickwelfloresvaldez <rhickwelfloresvaldez@gmail.com>
2025-02-03 10:30:18 +0800

    fix navbar display and scrolling issue on mobile nav

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function showMobileNav() {
     const mobileNav = document.querySelector('.mobile-nav');
     mobileNav.style.display = 'flex';
     mobileNav.classList.add('slide-in-top');
+    document.body.classList.add('no-scroll');
 }
 
 function hideMobileNav() {
@@ -11,5 +12,6 @@ function hideMobileNav() {
     mobileNav.addEventListener('animationend', function () {
         mobileNav.style.display = 'none';
         mobileNav.classList.remove('slide-out-top');
+        document.body.classList.remove('no-scroll');
     }, { once: true });
 }
